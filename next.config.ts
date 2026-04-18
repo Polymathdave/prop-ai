1bceb5c0a2766bdcbbeaa1b90015f2e0f70a8b1c
David Olawale Makinde <davidolawalemakinde136@gmail.com>
2026-04-18 11:16:51 +0100

    feat: add remote image patterns for Unsplash and custom R2 storage in Next.js configuration

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,18 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-};
 
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'images.unsplash.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'pub-e0ef2403878e4f15bc76e140f6eb8081.r2.dev',
+      },
+    ],
+  },
+};
 export default nextConfig;
